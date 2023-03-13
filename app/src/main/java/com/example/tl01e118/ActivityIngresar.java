0c2ad37b8313ebd506873274fa08fca527589f8b
ronOsLo <92495345+ronOsLo@users.noreply.github.com>
2023-03-12 19:24:34 -0600

    ver0.1

--- app/src/main/java/com/example/tl01e118/ActivityIngresar.java
+++ app/src/main/java/com/example/tl01e118/ActivityIngresar.java
@@ -3,15 +3,23 @@ package com.example.tl01e118;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.example.tl01e118.configuraciones.SQLliteConexion;
+import com.example.tl01e118.configuraciones.transacciones;
 
 public class ActivityIngresar extends AppCompatActivity {
     Spinner pais;
+    String Cpais;
     EditText nombre, numero, nota;
     Button guardar, contactos;
 
@@ -31,11 +39,70 @@ public class ActivityIngresar extends AppCompatActivity {
         ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource(this,R.array.combo_Pais, android.R.layout.simple_spinner_item);
     pais.setAdapter(adapter);
 
-    guardar.setOnClickListener(new View.OnClickListener() {
+    contactos.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View view) {
+            Intent lista = new Intent(getApplicationContext(),ActivityList.class);
+            startActivity(lista);
+        }
+    });
+
+    guardar.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view)
+        {
+            if(nombre.getText().length()==0)
+            {
+                Toast.makeText(getApplicationContext(),"Debe ingresar un Nombre ", Toast.LENGTH_SHORT).show();
+            } else if (numero.getText().length()==0)
+            {
+                Toast.makeText(getApplicationContext(),"Debe ingresar un Numero ", Toast.LENGTH_SHORT).show();
+            } else if (nota.getText().length()==0)
+            {
+                Toast.makeText(getApplicationContext(),"Debe ingresar una Nota ", Toast.LENGTH_SHORT).show();
+            } else
+            {
+                Cpais=pais.getSelectedItem().toString();
+                guardarContacto();
+
+            }
 
         }
     });
+    }
+
+    private void guardarContacto()
+    {
+        try {
+            SQLliteConexion conexion = new SQLliteConexion(this,
+                    transacciones.NameDatabase,
+                    null,
+                    1);
+            SQLiteDatabase db =conexion.getWritableDatabase();
+            ContentValues valores =new ContentValues();
+            valores.put(transacciones.pais, Cpais.toString());
+            valores.put(transacciones.nombre, nombre.getText().toString());
+            valores.put(transacciones.numero, numero.getText().toString());
+            valores.put(transacciones.nota, nota.getText().toString());
+
+            long resultado = db.insert(transacciones.tablaagenda, transacciones.id, valores);
+
+            Toast.makeText(this, "INGRESADO CORRECTAMENTE", Toast.LENGTH_SHORT).show();
+            limpiarPantalla();
+        }catch (Exception er)
+        {
+            er.toString();
+            Toast.makeText(this,er.toString(), Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
+
+    private void limpiarPantalla()
+    {
+
+        nombre.setText("");
+        numero.setText("");
+        nota.setText("");
     }
 }
--- app/src/main/java/com/example/tl01e118/ActivityList.java
+++ app/src/main/java/com/example/tl01e118/ActivityList.java
@@ -2,13 +2,72 @@ package com.example.tl01e118;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.example.tl01e118.configuraciones.SQLliteConexion;
+import com.example.tl01e118.configuraciones.transacciones;
+import com.example.tl01e118.tablas.contactos;
+
+import java.util.ArrayList;
 
 public class ActivityList extends AppCompatActivity {
+SQLliteConexion conexion;
+ListView listaContactos;
+ArrayList<contactos> lista;
+ArrayList<String> arregloContactos;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+
+        conexion = new SQLliteConexion(this, transacciones.NameDatabase,null,1);
+        listaContactos=(ListView) findViewById(R.id.listaContactos);
+
+        obtenerListaContactos();
+
+        ArrayAdapter adp = new ArrayAdapter(this, android.R.layout.simple_list_item_1,arregloContactos);
+        listaContactos.setAdapter(adp);
+
+    }
+
+    private void obtenerListaContactos()
+    {
+        SQLiteDatabase db =conexion.getReadableDatabase();
+        contactos contac =null;
+        lista=new ArrayList<contactos>();
+
+        Cursor cursor = db.rawQuery("select * from "+ transacciones.tablaagenda,null);
+        while (cursor.moveToNext())
+        {
+           contac=new contactos();
+           contac.setId(cursor.getInt(0));
+           contac.setPais(cursor.getString(1));
+           contac.setNombre(cursor.getString(2));
+           contac.setNumero(cursor.getString(3));
+           contac.setNota(cursor.getString(4));
+           lista.add(contac);
+        }
+        cursor.close();
+        filllist();
+
+    }
+
+    private void filllist()
+    {
+        arregloContactos=new ArrayList<String>();
+        for (int i=0; i<lista.size();i++)
+        {
+            arregloContactos.add(lista.get(i).getId()+" | "+
+                    lista.get(i).getPais()+" | "+
+                    lista.get(i).getNombre()+" | "+
+                    lista.get(i).getNumero()+" | "+
+                    lista.get(i).getNota());
+        }
     }
 }
